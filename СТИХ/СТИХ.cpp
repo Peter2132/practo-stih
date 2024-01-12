1bd76ea1e9c6d3d9abdc523fc9d4f3b2cc854e0b
Сергей Трофимов <isip_p.s.trofimov@mpt.ru>
2024-01-12 18:40:19 +0300

    ghfrn

---
+++ СТИХ/СТИХ.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    std::cout << "Бездомная Лягушка пела - квакала,\n";
+    cout << "Печально пела и при этом плакала.\n";
+    cout << "Наплакала Лягушка целый пруд.\n";
+    cout << "С тех самых пор лягушки в нём живут.\n";
+}
